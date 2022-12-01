be62d7f90d8747714cc723ed8d1d8582768015c3
Evgeniy <zheshe@list.ru>
2022-12-01 11:46:08 +0300

    renamed the variables

--- Task_2.java
+++ Task_2.java
@@ -15,7 +15,7 @@ public class Task_2 {
         System.out.println(number + " -> " + romanToInt(number));
     }
 
-    private static int romanToInt(String arabNumber) {
+    private static int romanToInt(String romanNumber) {
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -24,15 +24,15 @@ public class Task_2 {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-        int romanNumber = map.get(arabNumber.charAt(arabNumber.length() - 1));
+        int arabNumber = map.get(romanNumber.charAt(romanNumber.length() - 1));
 
-        for (int i = arabNumber.length() - 2; i >= 0; i--) {
-            int currentValue = map.get(arabNumber.charAt(i));
-            int nextValue = map.get(arabNumber.charAt(i + 1));
+        for (int i = romanNumber.length() - 2; i >= 0; i--) {
+            int currentValue = map.get(romanNumber.charAt(i));
+            int nextValue = map.get(romanNumber.charAt(i + 1));
 
-            if (currentValue < nextValue) romanNumber -= currentValue;
-            else romanNumber += currentValue;
+            if (currentValue < nextValue) arabNumber -= currentValue;
+            else arabNumber += currentValue;
         }
-        return romanNumber;
+        return arabNumber;
     }
 }
